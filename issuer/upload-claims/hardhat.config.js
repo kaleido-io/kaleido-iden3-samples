3e368ef6c26ec7c4e4be3a570c1649e4d0d445b7
Jim Zhang <jim.zhang@kaleido.io>
2022-12-06 11:52:35 -0500

    Update license and add copyright headers

--- issuer/upload-claims/hardhat.config.js
+++ issuer/upload-claims/hardhat.config.js
@@ -5,15 +5,11 @@ require('@openzeppelin/hardhat-upgrades');
 module.exports = {
   networks: {
     kaleido: {
-      url: `${process.env.KALEIDO_NODE_URL}`,
-      // gas: 10000000,
-      // httpHeaders: {
-      //   Authorization: `Basic ${process.env.BASIC_AUTH}`,
-      // },
+      url: process.env.KALEIDO_NODE_URL,
     },
     mumbai: {
-      url: 'http://ec2-18-117-125-203.us-east-2.compute.amazonaws.com:8545',
-      accounts: ['0x570ca3f6374b7709e68a5f96e25dab2459a7283064b58ad86c0aee9a26ceef70'],
+      url: process.env.MUMBAI_NODE_URL,
+      accounts: [process.env.MUMBAI_PRIV_KEY],
     },
   },
   solidity: {
--- issuer/upload-claims/scripts/upload-state-transition.js
+++ issuer/upload-claims/scripts/upload-state-transition.js
@@ -1,4 +1,19 @@
-const prompt = require('prompt-sync')();
+// Copyright © 2022 Kaleido, Inc.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 const { utils } = require('ffjavascript');
 const { unstringifyBigInts } = utils;
 const hre = require('hardhat');
@@ -55,7 +70,7 @@ async function main() {
   console.log('State after transaction: ', identityState1);
 }
 
-// copied from snarkjs.groth16.exportSolidityCallData()
+// modified from snarkjs.groth16.exportSolidityCallData()
 async function groth16ExportSolidityCallData(_proof, _pub) {
   const proof = unstringifyBigInts(_proof);
   const pub = unstringifyBigInts(_pub);
