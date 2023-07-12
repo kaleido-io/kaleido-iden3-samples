295212bd6c22c43b366a8290fb3eacd5195d6732
Jim Zhang <jim.zhang@kaleido.io>
2023-07-12 12:13:08 -0400

    local settings for working with didservice in EAP

--- v2/holder/wallet/lib/config.js
+++ v2/holder/wallet/lib/config.js
@@ -3,12 +3,12 @@ module.exports = {
     issuerServerUrl: 'http://localhost:3001',
   },
   kaleido: {
-    contractAddress: '0xd94Dae61E4A337C526527a79BB222f67C2fB6B81',
+    contractAddress: '0x81Fe83278D501457281dbd9d16FB9046E3Ae059d',
     url: {
       url: 'https://<environment ID>-<node ID>-rpc.us0-aws.kaleido.io',
       user: '<appcreds name>',
       password: '<appcreds password>',
     },
-    privateKey: '11abcdef01f1cd9bbabcdef12a34a4be005d276b3e89eea0a6e38440be660956',
+    privateKey: '11cfeaf8f0f1cd9bb575aac75a34a4be005d276b3e89eea0a6e38440be660956',
   },
 };
